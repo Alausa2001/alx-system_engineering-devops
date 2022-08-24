c9dbfde5bf3aecb8a3cf7555ca1df43be894ddcf
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-08-24 02:23:04 +0100

    finds the sender, the receiver and flags in a log file(Textme app)

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:([A-Za-z0-9+]+)\]\s\[to:([\w]+)\]\s\[flags:([-:\d]+)/).join(',')
+puts ARGV[0].scan(/from:([A-Za-z0-9+]+)\]\s\[to:([+\w]+)\]\s\[flags:([-:\d]+)/).join(',')
