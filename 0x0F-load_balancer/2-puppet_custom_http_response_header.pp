8c7edee14e05641eb729eb07d545912aca5b7dd7
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-27 12:47:46 +0100

    creating a custom HTTP header response, but with Puppe

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,20 @@
-# install haproxy and configure it to distribute traffic between two servers using roundrobin algorithm
-exec { 'update':
+# creating a custom HTTP header response, but with Puppet
+
+exec {'update':
   command  => 'sudo apt-get -y update',
   provider => 'shell'
 }
 
-package { 'haproxy':
+package {'nginx':
   ensure => 'installed'
 }
 
-exec { 'frontend':
-  command  => 'sudo sed -e "34a frontend http\n\tbind *:80\n\tmode http\n\tdefault_backend feranmi_backend" -i /etc/haproxy/haproxy.cfg',
-  provider => 'shell'
-}
-$bckend = 'backend feranmi_backend\n\tbalance roundrobin\n\tserver 34482-web-01 18.204.227.153:80 check'
-exec {'backend':
-  command  => "sudo sed -e '38a ${bckend}' -i /etc/haproxy/haproxy.cfg",
+exec {'customheader':
+  command  => "sudo sed -e '52i \\\t\tadd_header X-Served-By ${hostname};' -i /etc/nginx/sites-available/default;",
   provider => 'shell'
 }
 
-exec {'backend2':
-  command  => "sudo sed -e '41a \\\tserver 34482-web-02 34.232.53.153:80 check' -i /etc/haproxy/haproxy.cfg",
-  provider => 'shell'
-}
-
-exec {'service':
-  command  => 'sudo service haproxy restart',
+exec {'restart':
+  command  => 'sudo service nginx restart',
   provider => 'shell'
 }
