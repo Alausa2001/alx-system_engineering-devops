a30704d16e49e47834fb80e27379cac2908ea9d6
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-11-17 16:02:00 +0100

    Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Python script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+
+Requirements:
+
+    You must use urllib or requests module
+    The script must accept an integer as a parameter, which is the employee ID
+    The script must display on the standard output the employee TODO list
+    progress in this exact format:
+        First line: Employee EMPLOYEE_NAME is done with
+        tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+            EMPLOYEE_NAME: name of the employee
+            NUMBER_OF_DONE_TASKS: number of completed tasks
+            TOTAL_NUMBER_OF_TASKS: total number of tasks,
+            which is the sum of completed and non-completed tasks
+    Second and N next lines display the title of completed tasks:
+    TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+"""
+import requests as req
+from sys import argv as av
+
+
+def getInfo():
+    """gets information about an employees' TODO list progress."""
+    base = 'https://jsonplaceholder.typicode.com/'
+    # get employees personal info
+    info = req.get(base + 'users/{}'.format(av[1])).json()
+    todos = req.get(base + 'users/{}/todos'.format(av[1])).json()
+    allTodo = len(todos)
+    completedTasks = []
+    count = 0
+    for todo in todos:
+        if todo['completed'] is True:
+            count += 1
+            completedTasks.append(todo['title'])
+    print('Employee {} is done with tasks({}/{})'
+          .format(info['name'], count, allTodo))
+    for task in completedTasks:
+        print('\t' + task)
+
+
+if __name__ == '__main__':
+    getInfo()
