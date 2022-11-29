2c07a0f7e4e47f8646179e785ed66eb048d01bff
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-11-30 00:56:54 +0100

    queries Reddit API to get the number of top 10 posts of a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""queries Reddit API to get the number of top 10 posts of a subreddit"""
+
+import requests as req
+
+
+def top_ten(subreddit):
+    """get the top 10 posts of a subreddit"""
+    base = 'https://www.reddit.com'
+    header = {'User-agent': 'a_oluwaferanmi'}
+    respons = req.get('{}/r/{}/top.json'
+                      .format(base, subreddit),
+                      headers=header, allow_redirects=False)
+    if respons.status_code != 200:
+        return 0
+    posts = respons.json().get('data').get('children')
+    count = 0
+    for post in posts:
+        if count <= 9:
+            print(post.get('data').get('title'))
+            count += 1
