1c7a68d3cc0c5d699ffe42caef82abcf44a77a42
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-11-30 03:43:19 +0100

    queries Reddit API to get all hot articles of a subreddit recursively

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,17 +8,19 @@ def recurse(subreddit, hot_list=[], after=''):
     """get all the hot articles of a subreddit"""
     base = 'https://www.reddit.com'
     header = {'User-agent': 'a_oluwaferanmi'}
-    respons = req.get('{}/r/{}/hot.json?after={}&limit=100'
+    respons = req.get('{}/r/{}/hot.json?after={}&limit=200'
                       .format(base, subreddit, after),
                       headers=header, allow_redirects=False)
     if respons.status_code != 200:
         return None
-    else:
-        posts = respons.json().get('data').get('children')
-        for post in posts:
-            hot_list.append(post.get('data').get('title'))
+    posts = respons.json().get('data').get('children')
+    _next = respons.json().get('data').get('after')
+    for post in posts:
+        hot_list.append(post.get('data').get('title'))
 
-    if after is not None:
-        _next = respons.json().get('data').get('after')
+    if _next is not None:
         recurse(subreddit, hot_list, _next)
+        # print (f'Next {_next}')
+        # print(f'after {after}')
+        # print(_next == after)
     return hot_list
